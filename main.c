a445452e430b2f443b08540863f9e543875298e8
Sl33pySage <jasonwolf82@hotmail.com>
2025-11-06 23:07:49 -0600

    On Step 3

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+#include <netinet/in.h>
+#include <netinet/ip.h>
 #include <sys/socket.h>
 
-int main() { int fd = socket(AF_INET, SOCK_STREAM, 0); }
+int main() {
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  int val = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(1234);     // port
+  addr.sin_addr.s_addr = htonl(0); // wildcard IP 0.0.0.0
+  int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+  if (rv) {
+    die("bind()");
+  }
+}
