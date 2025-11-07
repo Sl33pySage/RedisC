e6f98056c805fa26554c2296d89e4ad5b61a549d
Sl33pySage <jasonwolf82@hotmail.com>
2025-11-07 13:46:08 -0600

    On SideNote:IPv6 address

--- main.c
+++ main.c
@@ -24,4 +24,11 @@ int main() {
   struct in_addr {
     uint32_t s_addr; // IPv4 in big-endian
   };
+
+  // uint32_t htonl(uint32_t hostlong);  CPU endian <=> big-endian
+  // uint16_t htons(uint16_t hostshort); same; 16-bit
+  // uint32_t htonl(uint32_t hostlong);
+  // uint32_t ntohl(uint32_t netlong); n-to-h is the same as h-to-a
+  // uint32_t htobe32(uint32_t host_32bits); "be" stands for big-endian
+  // uint32_t be32toh(uint32_t big_endian_32bits);
 }
